3aeac691a35338c85f3684db267c4158d63150af
Hitallo Azevedo <azevedohitallo@gmail.com>
2024-05-25 23:33:46 -0300

    feat: animação do menu, e animação do header ao realizar scroll

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,16 @@ import './style.css'
 
 const Header = () => {
     const [estadoMenu, setEstadoMenu] = useState('deactivated');
+    const [scrollState, setScrollState] = useState(0)
+
+    window.addEventListener('scroll', () => {
+      console.log(window.scrollY)
+      if (window.scrollY > 750){
+        setScrollState(1)
+      } else {
+        setScrollState(0)
+      }
+    })
 
     const menuIconRef = useRef<HTMLElement>(null);
 
@@ -19,17 +29,23 @@ const Header = () => {
       }
     };
 
+    const hideMenu = () => {
+      const element = menuIconRef.current;
+      setEstadoMenu('deactivated')
+      element?.classList.toggle('fa-x')
+      element?.classList.toggle('fa-bars')
+    }
     return (
-        <div className="header">
+        <div className={`header ${scrollState == 1 ? 'scroll' : ''}`}>
             <i className="fa-solid fa-bars" id='menuIcon' ref={menuIconRef} onClick={handleClick}></i>
-
+            <h1 className={`${scrollState == 1 ? 'scrolltitle' : ''}`}>AROMAS DO NORDESTE</h1>
             <div className={`menu ${estadoMenu}`}>
                 <nav>
                     <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Sobre nós</a></li>
-                        <li><a href="#">Cardapio</a></li>
-                        <li><a href="#">Fazer pedido</a></li>
+                        <li><a href="#home" onClick={hideMenu}>Início</a></li>
+                        <li><a href="#aboutus" onClick={hideMenu}>Sobre nós</a></li>
+                        <li><a href="#" onClick={hideMenu}>Cardapio</a></li>
+                        <li><a href="#" onClick={hideMenu}>Fazer pedido</a></li>
                     </ul>
                 </nav>
             </div>
