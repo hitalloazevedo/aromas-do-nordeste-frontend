fb58ec9aab6278ada3cf2e3cb287e76c9b214c90
Hitallo Azevedo <azevedohitallo@gmail.com>
2024-05-27 23:47:59 -0300

    feat: finalizando componente header, funcionalidades, e animações

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,49 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './style.css'
+import './media.css'
 
 const Header = () => {
-    const [estadoMenu, setEstadoMenu] = useState('deactivated');
+    const [estadoMenu, setEstadoMenu] = useState('');
     const [scrollState, setScrollState] = useState(0)
+    const [viewportHeight, setViewPortHeight] = useState(window.visualViewport?.height || Number(window.visualViewport?.height))
+    const [viewportWidth, setViewPortWidth] = useState(window.visualViewport?.width || Number(window.visualViewport?.width))
+    const [estadoNav, setEstadoNav] = useState('deactivatedNav')
 
+    
+    window.addEventListener("DOMContentLoaded", () => {
+      setViewPortHeight(Number(window.visualViewport?.height))
+      setViewPortWidth(Number(window.visualViewport?.width))
+      if (viewportWidth > 1000){
+        setEstadoMenu('')
+      } else {
+        setEstadoMenu('deactivated')
+      }
+    })
+    
+    window.addEventListener('resize', () => {
+      setViewPortWidth(Number(window.visualViewport?.width))
+      if (viewportWidth > 1000){
+        setEstadoMenu('')
+      } else {
+        setEstadoMenu('deactivated')
+        setEstadoNav('deactivatedNav')
+      }
+    })
+
+    useEffect(() => {
+      if (viewportWidth < 1000){
+        setEstadoMenu('deactivated')
+      }
+    }, [])
+    
     window.addEventListener('scroll', () => {
-      console.log(window.scrollY)
-      if (window.scrollY > 750){
+      if (window.scrollY > (viewportHeight - (viewportHeight * 0.18))){
         setScrollState(1)
       } else {
         setScrollState(0)
       }
     })
-
+    
     const menuIconRef = useRef<HTMLElement>(null);
 
     const handleClick = () => {
@@ -23,17 +53,22 @@ const Header = () => {
         element.classList.toggle('fa-x');
         if (element.classList.contains('fa-x')) {
           setEstadoMenu('activated');
+          setEstadoNav('activatedNav')
         } else {
           setEstadoMenu('deactivated');
+          setEstadoNav('deactivatedNav')
         }
       }
     };
 
     const hideMenu = () => {
       const element = menuIconRef.current;
-      setEstadoMenu('deactivated')
-      element?.classList.toggle('fa-x')
-      element?.classList.toggle('fa-bars')
+      if (viewportWidth < 1000){
+        setEstadoMenu('deactivated')
+        setEstadoNav('deactivatedNav')
+        element?.classList.toggle('fa-x')
+        element?.classList.toggle('fa-bars')
+      }
     }
     return (
         <div className={`header ${scrollState == 1 ? 'scroll' : ''}`}>
@@ -42,10 +77,10 @@ const Header = () => {
             <div className={`menu ${estadoMenu}`}>
                 <nav>
                     <ul>
-                        <li><a href="#home" onClick={hideMenu}>Início</a></li>
-                        <li><a href="#aboutus" onClick={hideMenu}>Sobre nós</a></li>
-                        <li><a href="#" onClick={hideMenu}>Cardapio</a></li>
-                        <li><a href="#" onClick={hideMenu}>Fazer pedido</a></li>
+                        <li><a href="#home" onClick={hideMenu} className={estadoNav}>Início</a></li>
+                        <li><a href="#aboutus" onClick={hideMenu} className={estadoNav}>Sobre nós</a></li>
+                        <li><a href="#" onClick={hideMenu} className={estadoNav}>Cardapio</a></li>
+                        <li><a href="#" onClick={hideMenu} className={estadoNav}>Fazer pedido</a></li>
                     </ul>
                 </nav>
             </div>
