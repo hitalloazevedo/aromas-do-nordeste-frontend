f6a0f7c064a1ae80a9659a3cec821a5ad2654fa8
Hitallo Azevedo <azevedohitallo@gmail.com>
2024-05-27 23:45:31 -0300

    style: finalizando a pagina home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -19,17 +19,17 @@ const Home = () => {
                 </div>
             </div>
             <div id="aboutus">
-                <header>
+                <div className='title'>
                     <h2>Sobre nós</h2>
-                </header>
-                <body>
+                </div>
+                <div className='content'>
                     <p>
                     Bem-vindo ao Aromas do Nordeste, onde a tradição e o sabor se encontram para proporcionar uma experiência gastronômica inesquecível. Nosso restaurante celebra a rica cultura e culinária do Nordeste brasileiro, trazendo à sua mesa os sabores autênticos e as receitas de família que passam de geração em geração.
                     No Aromas do Nordeste, você encontrará um ambiente acolhedor e autêntico, perfeito para reunir amigos e familiares. Nossa equipe dedicada se orgulha em oferecer pratos preparados com ingredientes frescos e selecionados, sempre respeitando as tradições culinárias nordestinas.
                     Deixe-se levar pelo aroma irresistível do nosso cuscuz, a maciez da carne de sol e o sabor inigualável do acarajé. Cada prato é uma homenagem à diversidade e riqueza cultural do Nordeste, preparado com carinho e dedicação para proporcionar uma verdadeira viagem gastronômica.
                     Venha nos visitar e descubra por que o Aromas do Nordeste é o lugar ideal para quem busca uma refeição deliciosa e uma experiência cultural autêntica. Aqui, cada mordida é uma celebração do nosso querido Nordeste.
                     </p>
-                </body>
+                </div>
             </div>
             <div id="footer">
                 <p>E aí, já decidiu?</p>
