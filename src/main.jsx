cbec43c96df0470ecbc823917d16ec3436f5ee35
Hitallo Azevedo <azevedohitallo@gmail.com>
2024-05-29 15:15:39 -0300

    removendo o hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './globals.css'
-import { HashRouter, RouterProvider } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import router from './routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <HashRouter router={router}/>
+    <RouterProvider router={router}/>
 )
