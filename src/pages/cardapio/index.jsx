fe37cb7d3989b2727e280aa0094a9210abcb52a4
Hitallo Azevedo <azevedohitallo@gmail.com>
2024-05-29 00:19:13 -0300

    feat: pagina cardapio com renderização dinamica

---
+++ src/pages/cardapio/index.jsx
@@ -0,0 +1,43 @@
+import './style.css'
+import CardPrato from '../../components/CardPrato';
+import Header from '../../components/Header'
+import Footer from '../../components/Footer';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const baseUrl = 'http://localhost:3000'
+
+const Cardapio = () => {
+
+    const [data, setData] = useState()
+    
+    const getPlates = async () => {
+        axios.get(`${baseUrl}/cardapio`).then(response => {
+            setData(response.data)
+        })
+    }
+    
+    useEffect(() => {
+        async function a(){
+            await getPlates()
+        }
+        a()
+    }, [])
+
+    return(
+        <>
+            <Header option={true}></Header>
+            <div className='cardapio'>
+                <h1>Cardápio</h1>
+                <div className="container">
+                    {data != undefined ? data.map((plate) => {
+                        return <CardPrato nomePrato={plate.name} imageUrl={plate.imageUrl} descricao={plate.description}/>
+                    }) : ''}
+                </div>
+            </div>
+            <Footer/>
+        </>
+    )
+}
+
+export default Cardapio;
