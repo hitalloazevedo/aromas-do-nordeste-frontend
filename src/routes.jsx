291af15b8d7ccb607da999a96078481dc5b98170
Hitallo Azevedo <azevedohitallo@gmail.com>
2024-05-28 21:35:53 -0300

    feat: adiconando a rota /dashboard

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/home";
 import Cardapio from "./pages/cardapio";
+import Dashboard from "./pages/dashboard";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
     {
         path: '/cardapio',
         element: <Cardapio/>
+    },
+    {
+        path: '/dashboard',
+        element: <Dashboard/>
     }
 ])
 
