5d13b7f5f1d39350b9a79d33e7f95096a111d761
Hitallo Azevedo <azevedohitallo@gmail.com>
2024-05-29 14:56:37 -0300

    test: alterando o provider do router

--- src/main.jsx
+++ src/main.jsx
@@ -5,5 +5,5 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router}/>
+    <HashChangeEvent router={router}/>
 )
